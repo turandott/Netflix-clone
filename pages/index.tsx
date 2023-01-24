7e8f492651ba9fb0d87a599879a506b4dbe4de59
turandott <greyvilka@gmail.com>
2023-01-24 22:39:16 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home -- Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main>
+
+
+        <section>
+
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
